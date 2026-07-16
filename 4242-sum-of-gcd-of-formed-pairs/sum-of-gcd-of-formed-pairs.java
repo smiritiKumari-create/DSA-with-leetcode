dc58bedf9d3b348f3367194362835181e693c4bc
smiriti kumari <smiritik123@gmail.com>
2026-07-16 20:48:31 +0530

    Time: 53 ms (95.50%) | Memory: 107.8 MB (84.50%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 
 public class Solution {
-    // Helper method to calculate Greatest Common Divisor (GCD)
     private int gcd(int a, int b) {
         while (b != 0) {
             int temp = b;
@@ -16,8 +15,6 @@ public class Solution {
         int[] prefixGcd = new int[n];
         
         int currentMax = nums[0];
-        
-        // Step 1: Construct the prefixGcd array
         for (int i = 0; i < n; i++) {
             if (nums[i] > currentMax) {
                 currentMax = nums[i];
@@ -25,10 +22,8 @@ public class Solution {
             prefixGcd[i] = gcd(nums[i], currentMax);
         }
         
-        // Step 2: Sort the prefixGcd array in non-decreasing order
         Arrays.sort(prefixGcd);
         
-        // Step 3: Pair the smallest and largest elements using two pointers
         int left = 0;
         int right = n - 1;
         long totalGcdSum = 0;
